1b293f3e752c2ce3d475fa7238c1ff6a2f4d342b
Roberto A Moraes <endriosrobert@gmail.com>
2022-08-09 11:16:52 -0300

    first commit

---
+++ Work-Servico/src/main/java/com/poshyweb/workservico/WorkServicoApplication.java
@@ -0,0 +1,13 @@
+package com.poshyweb.workservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkServicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkServicoApplication.class, args);
+    }
+
+}
---
+++ Work-Servico/src/test/java/com/poshyweb/workservico/WorkServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poshyweb.workservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkServicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
