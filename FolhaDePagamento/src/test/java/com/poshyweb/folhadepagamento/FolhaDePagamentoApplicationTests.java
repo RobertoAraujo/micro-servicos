3fbdc8aebd379e4ddc2e6a9e0092a9be7ab888db
Roberto A Moraes <endriosrobert@gmail.com>
2022-08-09 13:29:56 -0300

    primeiro commit

---
+++ FolhaDePagamento/src/test/java/com/poshyweb/folhadepagamento/FolhaDePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poshyweb.folhadepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhaDePagamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
