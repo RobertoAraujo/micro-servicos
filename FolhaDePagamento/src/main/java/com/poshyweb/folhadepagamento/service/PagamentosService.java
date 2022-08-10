62d83f1061f848562dd2253c95e8f5aa04577eb1
Roberto A Moraes <endriosrobert@gmail.com>
2022-08-10 10:57:44 -0300

    usando orestTemplate

--- FolhaDePagamento/src/main/java/com/poshyweb/folhadepagamento/service/PagamentosService.java
+++ FolhaDePagamento/src/main/java/com/poshyweb/folhadepagamento/service/PagamentosService.java
@@ -1,20 +1,30 @@
 package com.poshyweb.folhadepagamento.service;
 
-import com.poshyweb.folhadepagamento.client.WorkerFingCleint;
 import com.poshyweb.folhadepagamento.dominio.entity.Pagamento;
 import com.poshyweb.folhadepagamento.dominio.entity.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PagamentosService {
 
+    @Value(value = "${hr-worker.hot}")
+    private String workerHost;
+
     @Autowired
-    private WorkerFingCleint workerFingCleint;
+    private RestTemplate restTemplate;
 
     public Pagamento getPagamento(Long workeId, int dia) {
-        Worker worker = workerFingCleint.buscarPorId(workeId).getBody();
+        Map<String, String> uriVariaveis = new HashMap<>();
+        uriVariaveis.put("id", ""+workeId);
+        Worker worker = restTemplate.getForObject(workerHost + "worke/{id}", Worker.class,uriVariaveis);
         assert worker != null;
         return  new Pagamento(worker.getNome(), worker.getSalario(), dia);
     }
+
 }
