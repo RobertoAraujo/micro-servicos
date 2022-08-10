680b4537a79f32c59db3dcdec873d6b536723aab
Roberto A Moraes <endriosrobert@gmail.com>
2022-08-10 14:44:44 -0300

    projeto erureca server

---
+++ src/main/java/com/poshyweb/hreurecaservice/HrEurecaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.poshyweb.hreurecaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurecaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurecaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poshyweb/hreurecaservice/HrEurecaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poshyweb.hreurecaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurecaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
